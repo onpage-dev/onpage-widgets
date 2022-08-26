617f1ddc4f2fde3df46b978b1cd779079a430f8b
Giacomo Rizzi <gufoes@gmail.com>
2022-08-26 12:29:57 +0200

    v1.0.1

---
+++ demo/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import lib from "../../lib/index";
+const thing_id = 1419;
+const schema_id = 16308;
+const resource_id = 4829790;
+</script>
+
+<template>
+  <div id="test">
+    <button @click="lib.openEditModal(thing_id, schema_id, resource_id)">
+      Apri
+    </button>
+  </div>
+</template>
